b21da632e109ae16f54bbdb6021f2c5377538be9
Carlos Cabrera Semidey <ccarlosalejandros@gmail.com>
2021-02-10 16:05:25 -0500

    Navbar and Button Components done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//components
+import Navbar from "../src/components/Navbar";
+
+//styles
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <h1>hola</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
